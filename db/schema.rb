e12213388fb32ea30d68e0c749e134b08912937d
Sasha Shevchenko <sasha.shevchenko.dk@gmail.com>
2021-11-22 17:37:40 +0100

    Added columns to the Restaurant model by creating a migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_162018) do
+ActiveRecord::Schema.define(version: 2021_11_22_163253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 2021_11_22_162018) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "instagram_url"
+    t.string "tripadvisor_url"
+    t.string "facebook_url"
     t.index ["email"], name: "index_restaurants_on_email", unique: true
     t.index ["reset_password_token"], name: "index_restaurants_on_reset_password_token", unique: true
   end
