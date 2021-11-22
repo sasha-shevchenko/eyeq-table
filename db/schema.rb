8aaecb39874c2715a2e17a2f04102ebe5d80e6ff
Sasha Shevchenko <sasha.shevchenko.dk@gmail.com>
2021-11-22 17:42:00 +0100

    Created Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_163253) do
+ActiveRecord::Schema.define(version: 2021_11_22_164105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "restaurant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_categories_on_restaurant_id"
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +39,5 @@ ActiveRecord::Schema.define(version: 2021_11_22_163253) do
     t.index ["reset_password_token"], name: "index_restaurants_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "restaurants"
 end
