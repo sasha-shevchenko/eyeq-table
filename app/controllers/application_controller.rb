f1eb41f9fa234203b6a105829b7f76cdb45a0747
pieroolivares <piero-olivares@hotmail.com>
2021-11-23 14:13:49 +0100

    new and edit registration added + application controller parameters done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_restaurant!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :instagram_url, :tripadvisor_url, :facebook_url])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :instagram_url, :tripadvisor_url, :facebook_url])
+  end
 end
