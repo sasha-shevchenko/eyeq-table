333c4b6009b32fa113dade9137c3c0a245ccd663
Emily Millard <emily_millard@live.co.uk>
2021-11-23 15:21:52 +0100

    dashboard controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :restaurants
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :categories, only: [:new, :create]
-  resources :dashboard, only: :create
+  resources :categories, only: [:create]
+  resources :dashboard, only: :show
 end
