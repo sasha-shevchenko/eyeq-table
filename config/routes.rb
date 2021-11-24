8751e5a80a491e5bc271a2d9c9f53e7813a50548
Emily Millard <emily_millard@live.co.uk>
2021-11-24 11:56:15 +0100

    moved new item form to new view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :categories, only: [:create, :destroy] do
-    resources :items, only: [:create, :destroy]
+    resources :items, only: [:new, :create, :destroy]
   end
   get '/dashboard', to: 'dashboards#show', as: :dashboard
   resources :restaurants, only: :update
