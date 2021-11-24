c66abcaf782b55aefd05f51b535975992a620078
Clemens Grolman <clemens@grolmanmail.com>
2021-11-24 15:27:02 +0100

    refactured and updated routes. created the view for restaurant.show.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,6 @@
 class RestaurantsController < ApplicationController
   skip_before_action :authenticate_restaurant!, only: :home
   def show
-    @restaurants = current_restaurant
     @cetegories = current_restaurant.categories
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :restaurants
+  resources :restaurants, only: [:show, :update] do
+    resources :categories, only: [:create]
+  end
+    resources :categories, except: [:create, :destroy, :index, :show, :edit, :new, :update] do
+      resources :items, except: [:create, :destroy]
+    end
+    resources :categories, only: [:destroy]
+    resources :items, only: [:destroy]
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :categories, only: [:create, :destroy] do
-    resources :items, only: [:create, :destroy]
-  end
   get '/dashboard', to: 'dashboards#show', as: :dashboard
-  resources :restaurants, only: :update
 end
