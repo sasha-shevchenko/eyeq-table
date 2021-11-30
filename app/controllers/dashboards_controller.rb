002c3db9f3f358dcb8fc0d49aa792f0a23931cd7
Sasha Shevchenko <sasha.shevchenko.dk@gmail.com>
2021-12-01 00:35:20 +0100

    Updated dashboards#overview with table-overview-plan-card, floor-plan.png & styling

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,5 +4,7 @@ class DashboardsController < ApplicationController
   end
 
   def overview
+    @ordered_items = @current_session.session_items.where(sent_to_kitchen: true).group_by{ |session_item| session_item.item_id }
+    # @timestamp = @current_session.session_items.created_at
   end
 end
